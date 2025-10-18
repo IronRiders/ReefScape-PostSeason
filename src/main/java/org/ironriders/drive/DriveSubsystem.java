19dc7ac48b1df0847cde2b5ee1dc84c7f28cf3ac
drive-red <software@ironiders.org>
2025-10-18 09:10:11 -0700

    invert, pid, and dashboard changes

--- src/main/java/org/ironriders/drive/DriveSubsystem.java
+++ src/main/java/org/ironriders/drive/DriveSubsystem.java
@@ -37,7 +37,8 @@ public class DriveSubsystem extends IronSubsystem {
 
   private SwerveDrive swerveDrive;
   private Vision vision;
-  private boolean invertStatus = false;
+  private boolean rotationInvert = false;
+  private boolean driveInvert = false;
 
   public Command pathfindCommand;
   public double controlSpeedMultipler = 1;
@@ -69,12 +70,12 @@ public class DriveSubsystem extends IronSubsystem {
         swerveDrive::getRobotVelocity,
         (speeds, feedforwards) -> swerveDrive.setChassisSpeeds(
             new ChassisSpeeds(
-                speeds.vxMetersPerSecond,
-                speeds.vyMetersPerSecond,
+                -speeds.vxMetersPerSecond,
+                -speeds.vyMetersPerSecond,
                 RobotUtils.clamp(
                     -SWERVE_MAXIMUM_ANGULAR_VELOCITY,
                     SWERVE_MAXIMUM_ANGULAR_VELOCITY,
-                    speeds.omegaRadiansPerSecond))),
+                    -speeds.omegaRadiansPerSecond))),
         DriveConstants.HOLONOMIC_CONFIG,
         robotConfig,
         () -> {
@@ -96,8 +97,8 @@ public class DriveSubsystem extends IronSubsystem {
     // vision.addPoseEstimates();
 
     debugPublish("vision has pose", vision.hasPose);
-    debugPublish("inversion status", invertStatus);
-    publish("Inverted?", invertStatus);
+    publish("Drive Inverted?", driveInvert);
+    publish("Rotation Inverted?", rotationInvert);
   }
 
   /**
@@ -111,9 +112,8 @@ public class DriveSubsystem extends IronSubsystem {
    *                      its own rotation.
    */
   public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
-    int invertMultiplier = invertStatus ? -1 : 1;
     swerveDrive.drive(
-        translation.times(invertMultiplier), rotation * invertMultiplier, fieldRelative, false);
+        translation.times(driveInvert ? -1 : 1), rotation * (rotationInvert ? -1 : 1), fieldRelative, false);
   }
 
   /** Fetch the DriveCommands instance */
@@ -148,12 +148,11 @@ public class DriveSubsystem extends IronSubsystem {
     swerveDrive.resetOdometry(new Pose2d(pose2d.getTranslation(), new Rotation2d(0)));
   }
 
-  public void switchInvertControl() {
-    invertStatus = !invertStatus;
-  }
-
-  public int getInversionStatus() {
-    return invertStatus ? -1 : 1;
+  public void switchRotation() {
+    rotationInvert = !rotationInvert;
+  }  
+  public void switchDrive() {
+    driveInvert = !driveInvert;
   }
 
   public void setSpeed(double speed) {

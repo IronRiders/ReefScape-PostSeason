2f6af11f1feafe73ce1d90a6998c6f9e92a847a0
a-mishmash <reldamail@icloud.com>
2025-10-28 16:47:38 -0700

    FORMATTING MY BELOVED

--- src/main/java/org/ironriders/drive/DriveSubsystem.java
+++ src/main/java/org/ironriders/drive/DriveSubsystem.java
@@ -24,140 +24,141 @@ import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 /**
- * The DriveSubsystem encompasses everything that the Swerve Drive needs to function. It keeps track
- * of the robot's position and angle, and uses the controller input to figure out how the individual
+ * The DriveSubsystem encompasses everything that the Swerve Drive needs to
+ * function. It keeps track
+ * of the robot's position and angle, and uses the controller input to figure
+ * out how the individual
  * modules need to turn and be angled.
  */
 public class DriveSubsystem extends IronSubsystem {
 
-  private DriveCommands commands;
+    private DriveCommands commands;
 
-  private SwerveDrive swerveDrive;
-  private Vision vision;
-  private boolean rotationInvert = false;
-  private boolean driveInvert = false;
+    private SwerveDrive swerveDrive;
+    private Vision vision;
+    private boolean rotationInvert = false;
+    private boolean driveInvert = false;
 
-  public Command pathfindCommand;
-  public double controlSpeedMultipler = 1;
+    public Command pathfindCommand;
+    public double controlSpeedMultipler = 1;
 
-  public DriveSubsystem() throws RuntimeException {
-    try {
-      swerveDrive =
-          new SwerveParser(SWERVE_JSON_DIRECTORY) // YAGSL reads from the deploy/swerve directory
-              .createSwerveDrive(SWERVE_DRIVE_MAX_SPEED);
-    } catch (
-        IOException e) { // instancing SwerveDrive can throw an error, so we need to catch that.
-      throw new RuntimeException("Error configuring swerve drive", e);
+    public DriveSubsystem() throws RuntimeException {
+        try {
+            swerveDrive = new SwerveParser(SWERVE_JSON_DIRECTORY) // YAGSL reads from the deploy/swerve directory
+                    .createSwerveDrive(SWERVE_DRIVE_MAX_SPEED);
+        } catch (IOException e) { // instancing SwerveDrive can throw an error, so we need to catch that.
+            throw new RuntimeException("Error configuring swerve drive", e);
+        }
+
+        commands = new DriveCommands(this);
+        this.vision = new Vision(swerveDrive);
+
+        swerveDrive.setHeadingCorrection(false);
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+        RobotConfig robotConfig = null;
+        try {
+            robotConfig = RobotConfig.fromGUISettings();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not load path planner config", e);
+        }
+
+        AutoBuilder.configure(
+                swerveDrive::getPose,
+                swerveDrive::resetOdometry,
+                swerveDrive::getRobotVelocity,
+                (speeds, feedforwards) -> swerveDrive.setChassisSpeeds(
+                        new ChassisSpeeds(
+                                -speeds.vxMetersPerSecond,
+                                -speeds.vyMetersPerSecond,
+                                RobotUtils.clamp(
+                                        -SWERVE_MAXIMUM_ANGULAR_VELOCITY,
+                                        SWERVE_MAXIMUM_ANGULAR_VELOCITY,
+                                        -speeds.omegaRadiansPerSecond))),
+                DriveConstants.HOLONOMIC_CONFIG,
+                robotConfig,
+                () -> {
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this);
+
+        GameState.setField(swerveDrive.field);
+        GameState.setRobotPose(() -> Optional.of(swerveDrive.getPose()));
     }
 
-    commands = new DriveCommands(this);
-    this.vision = new Vision(swerveDrive);
-
-    swerveDrive.setHeadingCorrection(false);
-    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
-
-    RobotConfig robotConfig = null;
-    try {
-      robotConfig = RobotConfig.fromGUISettings();
-    } catch (Exception e) {
-      throw new RuntimeException("Could not load path planner config", e);
+    @Override
+    public void periodic() {
+        // Report info to driver + update pose w/ vision
+        publish("vision updated ok", vision.updatePose());
+        publish("vision has pose", vision.hasPose);
+        publish("Drive Inverted?", driveInvert);
+        publish("Rotation Inverted?", rotationInvert);
     }
 
-    AutoBuilder.configure(
-        swerveDrive::getPose,
-        swerveDrive::resetOdometry,
-        swerveDrive::getRobotVelocity,
-        (speeds, feedforwards) ->
-            swerveDrive.setChassisSpeeds(
-                new ChassisSpeeds(
-                    -speeds.vxMetersPerSecond,
-                    -speeds.vyMetersPerSecond,
-                    RobotUtils.clamp(
-                        -SWERVE_MAXIMUM_ANGULAR_VELOCITY,
-                        SWERVE_MAXIMUM_ANGULAR_VELOCITY,
-                        -speeds.omegaRadiansPerSecond))),
-        DriveConstants.HOLONOMIC_CONFIG,
-        robotConfig,
-        () -> {
-          var alliance = DriverStation.getAlliance();
-          if (alliance.isPresent()) {
-            return alliance.get() == DriverStation.Alliance.Red;
-          }
-          return false;
-        },
-        this);
+    /**
+     * Vrrrrooooooooom brrrrrrrrr BRRRRRR wheeee BRRR brrrr VRRRRROOOOOOM ZOOOOOOM
+     * ZOOOOM WAHOOOOOOOOO
+     * WAHAHAHHA (Drives given a desired translation and rotation.)
+     *
+     * @param translation   Desired translation in meters per second.
+     * @param rotation      Desired rotation in radians per second.
+     * @param fieldRelative If not field relative, the robot will move relative to
+     *                      its own rotation.
+     */
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
+        swerveDrive.drive(
+                translation.times(driveInvert ? -1 : 1),
+                rotation * (rotationInvert ? -1 : 1),
+                fieldRelative,
+                false);
+    }
 
-    GameState.setField(swerveDrive.field);
-    GameState.setRobotPose(() -> Optional.of(swerveDrive.getPose()));
-  }
+    /** Fetch the DriveCommands instance */
+    public DriveCommands getCommands() {
+        return commands;
+    }
 
-  @Override
-  public void periodic() {
-    // Report info to driver + update pose w/ vision
-    publish("vision updated ok", vision.updatePose());
-    publish("vision has pose", vision.hasPose);
-    publish("Drive Inverted?", driveInvert);
-    publish("Rotation Inverted?", rotationInvert);
-  }
+    public void resetRotation() {
+        Pigeon2 pigeon2 = new Pigeon2(9);
+        swerveDrive.resetOdometry(
+                new Pose2d(
+                        swerveDrive.getPose().getTranslation(),
+                        new Rotation2d(
+                                pigeon2.getYaw(true).waitForUpdate(1).getValueAsDouble() * (Math.PI / 180f))));
+        pigeon2.close();
+    }
 
-  /**
-   * Vrrrrooooooooom brrrrrrrrr BRRRRRR wheeee BRRR brrrr VRRRRROOOOOOM ZOOOOOOM ZOOOOM WAHOOOOOOOOO
-   * WAHAHAHHA (Drives given a desired translation and rotation.)
-   *
-   * @param translation Desired translation in meters per second.
-   * @param rotation Desired rotation in radians per second.
-   * @param fieldRelative If not field relative, the robot will move relative to its own rotation.
-   */
-  public void drive(Translation2d translation, double rotation, boolean fieldRelative) {
-    swerveDrive.drive(
-        translation.times(driveInvert ? -1 : 1),
-        rotation * (rotationInvert ? -1 : 1),
-        fieldRelative,
-        false);
-  }
+    /** Fetch the SwerveDrive instance */
+    public SwerveDrive getSwerveDrive() {
+        return swerveDrive;
+    }
 
-  /** Fetch the DriveCommands instance */
-  public DriveCommands getCommands() {
-    return commands;
-  }
+    public Vision getVision() {
+        return vision;
+    }
 
-  public void resetRotation() {
-    Pigeon2 pigeon2 = new Pigeon2(9);
-    swerveDrive.resetOdometry(
-        new Pose2d(
-            swerveDrive.getPose().getTranslation(),
-            new Rotation2d(
-                pigeon2.getYaw(true).waitForUpdate(1).getValueAsDouble() * (Math.PI / 180f))));
-    pigeon2.close();
-  }
+    public Pose2d getPose() {
+        return this.swerveDrive.getPose();
+    }
 
-  /** Fetch the SwerveDrive instance */
-  public SwerveDrive getSwerveDrive() {
-    return swerveDrive;
-  }
+    /** Resets the Odometry to the current position */
+    public void resetOdometry(Pose2d pose2d) {
+        swerveDrive.resetOdometry(new Pose2d(pose2d.getTranslation(), new Rotation2d(0)));
+    }
 
-  public Vision getVision() {
-    return vision;
-  }
+    public void switchRotation() {
+        rotationInvert = !rotationInvert;
+    }
 
-  public Pose2d getPose() {
-    return this.swerveDrive.getPose();
-  }
+    public void switchDrive() {
+        driveInvert = !driveInvert;
+    }
 
-  /** Resets the Odometry to the current position */
-  public void resetOdometry(Pose2d pose2d) {
-    swerveDrive.resetOdometry(new Pose2d(pose2d.getTranslation(), new Rotation2d(0)));
-  }
-
-  public void switchRotation() {
-    rotationInvert = !rotationInvert;
-  }
-
-  public void switchDrive() {
-    driveInvert = !driveInvert;
-  }
-
-  public void setSpeed(double speed) {
-    controlSpeedMultipler = speed;
-  }
+    public void setSpeed(double speed) {
+        controlSpeedMultipler = speed;
+    }
 }
--- src/main/java/org/ironriders/drive/Vision.java
+++ src/main/java/org/ironriders/drive/Vision.java
@@ -5,74 +5,82 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import java.util.Optional;
-import org.LimelightHelpers;
+
+import org.ironriders.lib.LimelightHelpers;
+
 import swervelib.SwerveDrive;
 
 /**
- * Vision is not a subsystem. This class is a utility class for the DriveSubsystem and controls all
+ * Vision is not a subsystem. This class is a utility class for the
+ * DriveSubsystem and controls all
  * of the apriltag processing and pose estimation.
  *
- * <p>(Why is it not a subsystem? Because it doesn't need to be.)
+ * <p>
+ * (Why is it not a subsystem? Because it doesn't need to be.)
  */
 public class Vision {
 
-  private SwerveDrive swerveDrive = null;
-  private Pigeon2 pigeon = new Pigeon2(9);
-  public boolean hasPose = false;
+    private SwerveDrive swerveDrive = null;
+    private Pigeon2 pigeon = new Pigeon2(9);
+    public boolean hasPose = false;
 
-  public Vision(SwerveDrive drive) {
-    this.swerveDrive = drive;
-  }
+    public Vision(SwerveDrive drive) {
+        this.swerveDrive = drive;
+    }
 
-  /**
-   * Call on each perodic tick to update the swerve drive with any available vision data
-   *
-   * @returns true on success false on failure
-   */
-  public boolean updatePose() {
-    // First, tell Limelight your robot's current orientation
-    double robotYaw = pigeon.getYaw().getValueAsDouble();
-    // Apparently these zeros are ignored?
-    LimelightHelpers.SetRobotOrientation(
-        DriveConstants.LIMELIGHT_NAME, robotYaw, 0.0, 0.0, 0.0, 0.0, 0.0);
-    boolean rejectUpdate = false;
-    // Set the right coordinate system and get the vision measurement
-    Optional<Alliance> alliance = DriverStation.getAlliance();
-    LimelightHelpers.PoseEstimate limelightMeasurement = new LimelightHelpers.PoseEstimate();
-    if (alliance.isPresent()) {
-      if (alliance.get() == Alliance.Red) {
-        limelightMeasurement =
-            LimelightHelpers.getBotPoseEstimate_wpiRed(DriveConstants.LIMELIGHT_NAME);
-      }
-      if (alliance.get() == Alliance.Blue) {
-        limelightMeasurement =
-            LimelightHelpers.getBotPoseEstimate_wpiBlue(DriveConstants.LIMELIGHT_NAME);
-      }
-    } else {
-      // FMS not connected
-      rejectUpdate = true;
+    /**
+     * Call on each perodic tick to update the swerve drive with any available
+     * vision data
+     *
+     * @returns true on success false on failure
+     */
+    public boolean updatePose() {
+
+        // First, tell Limelight your robot's current orientation
+        double robotYaw = pigeon.getYaw().getValueAsDouble();
+
+        // Apparently these zeros are ignored?
+        LimelightHelpers.SetRobotOrientation(
+                DriveConstants.LIMELIGHT_NAME, robotYaw, swerveDrive.angularVelocityCoefficient, 0.0, 0.0, 0.0, 0.0);
+        boolean rejectUpdate = false;
+
+        // Set the right coordinate system and get the vision measurement
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        LimelightHelpers.PoseEstimate limelightMeasurement = new LimelightHelpers.PoseEstimate();
+
+        if (alliance.isPresent()) {
+            if (alliance.get() == Alliance.Red) {
+                limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiRed(DriveConstants.LIMELIGHT_NAME);
+            }
+            if (alliance.get() == Alliance.Blue) {
+                limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue(DriveConstants.LIMELIGHT_NAME);
+            }
+        } else {
+            // FMS not connected
+            rejectUpdate = true;
+        }
+
+        if (Math.abs(pigeon.getAngularVelocityZWorld().getValueAsDouble()) > 360) {
+            // Rotating too fast for a valid measurement
+            rejectUpdate = true;
+        }
+        if (limelightMeasurement.tagCount == 0) {
+            // There's nothing to update off of
+            rejectUpdate = true;
+        }
+        if (!rejectUpdate) {
+            // Add it to your pose estimator
+            swerveDrive.setVisionMeasurementStdDevs(
+                    VecBuilder.fill(
+                            DriveConstants.VISION_X_TRUST,
+                            DriveConstants.VISION_Y_TRUST,
+                            DriveConstants.VISION_ANGLE_TRUST));
+            swerveDrive.addVisionMeasurement(
+                    limelightMeasurement.pose, limelightMeasurement.timestampSeconds);
+            this.hasPose = true;
+            return true;
+        } else {
+            return false;
+        }
     }
-    if (Math.abs(pigeon.getAngularVelocityZWorld().getValueAsDouble()) > 360) {
-      // Rotating too fast for a valid measurement
-      rejectUpdate = true;
-    }
-    if (limelightMeasurement.tagCount == 0) {
-      // There's nothing to update off of
-      rejectUpdate = true;
-    }
-    if (!rejectUpdate) {
-      // Add it to your pose estimator
-      swerveDrive.setVisionMeasurementStdDevs(
-          VecBuilder.fill(
-              DriveConstants.VISION_X_TRUST,
-              DriveConstants.VISION_Y_TRUST,
-              DriveConstants.VISION_ANGLE_TRUST));
-      swerveDrive.addVisionMeasurement(
-          limelightMeasurement.pose, limelightMeasurement.timestampSeconds);
-      this.hasPose = true;
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
